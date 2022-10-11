038b29a241989a9bded3cfcb5da8793ca15a148e
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-10-11 07:59:21 +0000

    Cogida los import correctos

--- GameExectutor.java
+++ GameExectutor.java
@@ -1,4 +1,15 @@
-public class GameExetutor {
+import java.util.concurrent.locks.*;
+import java.util.Scanner;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class GameExectutor {
 
     public static void controlador(String[] args) {
         Lock lock = new ReentrantLock();
